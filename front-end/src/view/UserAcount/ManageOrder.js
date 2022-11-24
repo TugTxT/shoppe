c29feab278b6983b438c28464823dd99c2689b88
TugTxT <tungnt.txt.2k@gmail.com>
2022-11-24 13:09:52 +0700

    TxT

--- front-end/src/view/UserAcount/ManageOrder.js
+++ front-end/src/view/UserAcount/ManageOrder.js
@@ -7,17 +7,62 @@ import { Col, Row, Tabs } from "antd";
 import NoOrder from "../../component/ManageOrder/NoOrder";
 import Order from "../../component/ManageOrder/Order";
 import { DataOrder } from "../../data/DataOrder";
-import { useDispatch } from "react-redux";
-import { orderStatus } from "../../redux-toolkit/reducer/orderSliceReducer";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  orderList,
+  orderStatus,
+} from "../../redux-toolkit/reducer/orderSliceReducer";
+import {
+  selectOrderList,
+  selectOrderStatus,
+} from "../../redux-toolkit/selector/selector";
 
 function ManageOrder() {
   const [btnActive, setBtnActive] = useState("nav-btn");
+  const [tabStatus, setTabStatus] = useState(null);
+
+  const dataOrder = useSelector(selectOrderStatus);
+  const orderListData = useSelector(selectOrderList);
+
+  const getOrderrListData = (status) => {
+    dispatch(orderStatus(status)).then((res) => {
+      if (res.meta.requestStatus === "fulfilled") {
+        dispatch(orderList());
+      }
+    });
+  };
 
   const dispatch = useDispatch();
   useEffect(() => {
-    dispatch(orderStatus());
-    // console.log(dataUser);
+    getOrderrListData("Chờ vận chuyển");
   }, []);
+
+  useEffect(() => {
+    switch (Number(tabStatus)) {
+      case 1:
+        getOrderrListData("Chờ vận chuyển");
+        break;
+      case 2:
+        getOrderrListData("Chờ nhận hàng");
+        break;
+      case 3:
+        getOrderrListData("Chờ xác nhận");
+        break;
+      case 4:
+        getOrderrListData("Chờ đánh giá");
+        break;
+      case 5:
+        getOrderrListData("Hoàn thành");
+        break;
+      case 6:
+        getOrderrListData("Hủy");
+        break;
+
+      default:
+        getOrderrListData("Chờ vận chuyển");
+        break;
+    }
+  }, [tabStatus]);
 
   return (
     <ManageOrderStyle>
@@ -26,17 +71,20 @@ function ManageOrder() {
       </div>
       <div className="right">
         <div className="right-top">
-          <div className="n">
+          {/* <div className="n">
             <span className="num1">{UserAcountInfor[1].unpaidOrder}</span>
             <span className="num2">{UserAcountInfor[1].orderProcess}</span>
             <span className="num3">{UserAcountInfor[1].orderShipping}</span>
             <span className="num4">{UserAcountInfor[1].orderReceived}</span>
-          </div>
+          </div> */}
           <div className="title">Quản lý đơn hàng</div>
           <div className="tabb">
-            <Tabs defaultActiveKey="1">
-              <Tabs.TabPane tab="Chờ thanh toán" key="1">
-                {DataOrder.length !== 0 ? (
+            <Tabs
+              defaultActiveKey="1"
+              onChange={(value) => setTabStatus(value)}
+            >
+              <Tabs.TabPane tab={`Chờ vận chuyển ${""}`} key="1">
+                {dataOrder?.length !== 0 ? (
                   <div className="bg">
                     <div className="tabl">
                       <div className="row">
@@ -64,7 +112,7 @@ function ManageOrder() {
                         </Row>
                       </div>
                       <div>
-                        {DataOrder?.map((data, index) => (
+                        {orderListData?.map((data, index) => (
                           <Order orderData={data} no={index} />
                         ))}
                       </div>
@@ -74,8 +122,8 @@ function ManageOrder() {
                   <NoOrder />
                 )}
               </Tabs.TabPane>
-              <Tabs.TabPane tab="Đang xử lý" key="2">
-                {DataOrder.length !== 0 ? (
+              <Tabs.TabPane tab="Chờ nhận hàng" key="2">
+                {DataOrder?.length !== 0 ? (
                   <div className="bg">
                     <div className="tabl">
                       <div className="row">
@@ -103,7 +151,7 @@ function ManageOrder() {
                         </Row>
                       </div>
                       <div>
-                        {DataOrder?.map((data, index) => (
+                        {orderListData?.map((data, index) => (
                           <Order orderData={data} no={index} />
                         ))}
                       </div>
@@ -113,8 +161,8 @@ function ManageOrder() {
                   <NoOrder />
                 )}
               </Tabs.TabPane>
-              <Tabs.TabPane tab="Đang vận chuyển" key="3">
-                {DataOrder.length !== 0 ? (
+              <Tabs.TabPane tab="Chờ xác nhận" key="3">
+                {DataOrder?.length !== 0 ? (
                   <div className="bg">
                     <div className="tabl">
                       <div className="row">
@@ -142,7 +190,7 @@ function ManageOrder() {
                         </Row>
                       </div>
                       <div>
-                        {DataOrder?.map((data, index) => (
+                        {orderListData?.map((data, index) => (
                           <Order orderData={data} no={index} />
                         ))}
                       </div>
@@ -152,8 +200,8 @@ function ManageOrder() {
                   <NoOrder />
                 )}
               </Tabs.TabPane>
-              <Tabs.TabPane tab="Đã nhận" key="4">
-                {DataOrder.length !== 0 ? (
+              <Tabs.TabPane tab="Chờ đánh giá" key="4">
+                {DataOrder?.length !== 0 ? (
                   <div className="bg">
                     <div className="tabl">
                       <div className="row">
@@ -181,7 +229,85 @@ function ManageOrder() {
                         </Row>
                       </div>
                       <div>
-                        {DataOrder?.map((data, index) => (
+                        {orderListData?.map((data, index) => (
+                          <Order orderData={data} no={index} />
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <NoOrder />
+                )}
+              </Tabs.TabPane>
+              <Tabs.TabPane tab="Hoàn thành" key="5">
+                {DataOrder?.length !== 0 ? (
+                  <div className="bg">
+                    <div className="tabl">
+                      <div className="row">
+                        <Row>
+                          <Col span={2}>
+                            <div className="t no">STT</div>
+                          </Col>
+                          <Col span={8}>
+                            <div className="t name">Sản phẩm</div>
+                          </Col>
+                          <Col span={3}>
+                            <div className="t quantity">Số lượng</div>
+                          </Col>
+                          <Col span={4}>
+                            <div className="t price">Giá</div>
+                          </Col>
+                          <Col span={3}>
+                            <div className="t discount">Khuyến mại</div>
+                          </Col>
+                          <div className="c">
+                            <Col span={4}>
+                              <div className="t total">Tổng</div>
+                            </Col>
+                          </div>
+                        </Row>
+                      </div>
+                      <div>
+                        {orderListData?.map((data, index) => (
+                          <Order orderData={data} no={index} />
+                        ))}
+                      </div>
+                    </div>
+                  </div>
+                ) : (
+                  <NoOrder />
+                )}
+              </Tabs.TabPane>
+              <Tabs.TabPane tab="Hủy" key="6">
+                {DataOrder?.length !== 0 ? (
+                  <div className="bg">
+                    <div className="tabl">
+                      <div className="row">
+                        <Row>
+                          <Col span={2}>
+                            <div className="t no">STT</div>
+                          </Col>
+                          <Col span={8}>
+                            <div className="t name">Sản phẩm</div>
+                          </Col>
+                          <Col span={3}>
+                            <div className="t quantity">Số lượng</div>
+                          </Col>
+                          <Col span={4}>
+                            <div className="t price">Giá</div>
+                          </Col>
+                          <Col span={3}>
+                            <div className="t discount">Khuyến mại</div>
+                          </Col>
+                          <div className="c">
+                            <Col span={4}>
+                              <div className="t total">Tổng</div>
+                            </Col>
+                          </div>
+                        </Row>
+                      </div>
+                      <div>
+                        {orderListData?.map((data, index) => (
                           <Order orderData={data} no={index} />
                         ))}
                       </div>
